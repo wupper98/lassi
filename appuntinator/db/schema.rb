9e297abb13dda9aa4324a9961fcd83f4ba3a788f
ndriabecks <aandreani1998@gmail.com>
2020-05-19 16:42:46 +0200

    added oauth

--- appuntinator/db/schema.rb
+++ appuntinator/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200517105305) do
+ActiveRecord::Schema.define(version: 20200519141907) do
 
   create_table "appuntos", force: :cascade do |t|
     t.text "contenuto"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20200517105305) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
