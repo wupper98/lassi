b43be27450391ec6321bd7ee4500091c0c0856d3
ndriabecks <32883013+aandreani@users.noreply.github.com>
2020-05-22 14:35:09 +0200

    Added 2 gems and roles_mask

--- appuntinator/db/schema.rb
+++ appuntinator/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200519182002) do
+ActiveRecord::Schema.define(version: 20200522121913) do
 
   create_table "appuntos", force: :cascade do |t|
     t.text "contenuto"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20200519182002) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.integer "roles_mask"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
