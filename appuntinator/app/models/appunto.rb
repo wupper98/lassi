27a93a07a682007799c8ef89caf11c7b6661b778
wupper98 <57945395+wupper98@users.noreply.github.com>
2020-05-25 17:10:39 +0200

    cascade on users and appuntos

--- appuntinator/app/models/appunto.rb
+++ appuntinator/app/models/appunto.rb
@@ -1,6 +1,6 @@
 class Appunto < ApplicationRecord
     belongs_to :user
-    has_many :comments
+    has_many :comments, dependent: :delete_all
 
     validates :contenuto, :presence => true
 end
--- appuntinator/app/models/user.rb
+++ appuntinator/app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
-  has_many :comments
-  has_many :appuntos
+  has_many :comments, dependent: :delete_all
+  has_many :appuntos, dependent: :delete_all
 
   validates :username,
     presence: true,
